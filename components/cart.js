a9095269c66f76f70ce79b8591421f17aeba9679
Leticia ONeill <leticia_oneill@brown.edu>
2024-06-22 14:19:13 -0400

    add stripe

--- components/cart.js
+++ components/cart.js
@@ -1,4 +1,7 @@
-import {store, component} from 'https://cdn.jsdelivr.net/npm/reefjs@12/dist/reef.es.min.js';
+import {
+  store,
+  component,
+} from "https://cdn.jsdelivr.net/npm/reefjs@12/dist/reef.es.min.js";
 
 // Hold cart data
 let cart = store(JSON.parse(localStorage.getItem("cart")) || {});
@@ -16,17 +19,27 @@ function addToCart(id) {
  * Remove a photo from the cart
  * @param  {String} id The photo ID
  */
-function removeFromCart (id) {
-	delete cart[id];
-	localStorage.setItem('cart', JSON.stringify(cart));
+function removeFromCart(id) {
+  delete cart[id];
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+/**
+ * Empty cart
+ */
+function emptyCart() {
+  for (let key in cart) {
+    delete cart[key];
+  }
+  localStorage.removeItem("cart");
 }
 
 /**
  * Check if an item is in the cart
- * @param {String} id The Photo ID
- * @return {Boolean} If true, the item is in the cart
+ * @param  {String}  id The photo ID
+ * @return {Boolean}    If true, the item is in the cart
  */
-function inCart (id) {
+function inCart(id) {
   return cart[id];
 }
 
@@ -34,11 +47,22 @@ function inCart (id) {
  * Get the cart count HTML
  * @return {String} The cart count HTML string
  */
-function cartCountHTML () {
+function cartCountHTML() {
   return `${Object.keys(cart).length}`;
 }
 
-// Create cart count component
-component('#cart-count', cartCountHTML);
+/**
+ * Get data for just photos that are in the cart
+ * @param  {Array} photos All photos
+ * @return {Array}        Photos in the cart
+ */
+function getPhotosInCart(photos) {
+  return photos.filter(function (photo) {
+    return inCart(photo.id);
+  });
+}
 
-export { addToCart, removeFromCart, inCart };
+// Create cart count component
+component("#cart-count", cartCountHTML);
+
+export { addToCart, removeFromCart, emptyCart, inCart, getPhotosInCart };
---
+++ components/endpoints.js
@@ -0,0 +1,7 @@
+// The photos API endpoint
+let photosURL = "https://web-apps-serverless.letioneill.workers.dev";
+
+// The stripe API endpoint
+let stripeURL = "https://web-apps-stripe.letioneill.workers.dev";
+
+export { photosURL, stripeURL };
